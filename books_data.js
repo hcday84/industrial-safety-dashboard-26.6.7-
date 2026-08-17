172eee402be5d371265b02029d91ddd267051199
hcday84 <hcday84@github.com>
2026-08-17 10:15:32 +0900

    auto: Claude 작업 자동 커밋

--- books_data.js
+++ books_data.js
@@ -1395,7 +1395,7 @@ const REAL_BOOKS = {
   ],
 
   'JLPT': [
-    { title: 'JLPT 한 권으로 합격 N1', author: '시원스쿨 일본어연구소', publisher: '시원스쿨랩', price: 18000, originalPrice: 20000, discount: '10%', rating: 4.8, reviews: 3456, tags: ['베스트'], imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788965426073.jpg', pageUrl: 'https://search.kyobobook.co.kr/search?keyword=JLPT+한권으로합격+N1' },
+    { title: '해커스 JLPT N1(일본어능력시험) 한 권으로 합격', author: '해커스 JLPT 연구소', publisher: '해커스어학연구소', price: 24210, originalPrice: 26900, discount: '10%', rating: 4.8, reviews: 81, tags: ['베스트'], hasEbook: false, imageUrl: KB('S000203100592'), pageUrl: 'https://product.kyobobook.co.kr/detail/S000203100592' },
     { title: 'JLPT 한 권으로 합격 N2', author: '시원스쿨 일본어연구소', publisher: '시원스쿨랩', price: 16200, originalPrice: 18000, discount: '10%', rating: 4.8, reviews: 3210, tags: ['베스트'], imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788965423393.jpg', pageUrl: 'https://search.kyobobook.co.kr/search?keyword=JLPT+한권으로합격+N2' },
   ],
 
