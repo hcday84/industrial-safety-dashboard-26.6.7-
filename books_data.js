ba659f0d710b128057e17d2221eb60dea72e1cd8
hcday84 <hcday84@github.com>
2026-08-19 18:22:51 +0900

    auto: Claude 작업 자동 커밋

--- books_data.js
+++ books_data.js
@@ -1512,7 +1512,7 @@ const REAL_BOOKS = {
   '심리상담사': [
     { title: '심리상담과 치료의 이론과 실제', author: 'Gerald Corey', publisher: '학지사', price: 39000, originalPrice: 39000, discount: '0%', rating: 4.8, reviews: 3210, tags: ['베스트'], hasEbook: false, isbn: '9788962185706', imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788962185706.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000216797281' },
     { title: '상담심리학의 이론과 실제', author: '노안영', publisher: '학지사', price: 22000, originalPrice: 22000, discount: '0%', rating: 4.7, reviews: 1876, tags: ['베스트'], hasEbook: false, isbn: '9788999714948', imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788999714948.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000001643218' },
-    { title: '집단상담의 이론과 실제', author: '천성문 외', publisher: '학지사', price: 20000, originalPrice: 22000, discount: '10%', rating: 4.6, reviews: 1543, tags: ['추천'], hasEbook: false, imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788999732041.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000214304053' },
+    { title: '집단상담의 이론과 실제', author: '천성문 외', publisher: '학지사', price: 20000, originalPrice: 22000, discount: '10%', rating: 4.6, reviews: 1543, tags: ['추천'], hasEbook: false, isbn: '9788999732041', imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788999732041.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000214304053' },
   ],
 
   'TAT': [
