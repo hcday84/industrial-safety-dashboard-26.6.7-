92917e135d9946776d727f6404f7210f627250b1
hcday84 <hcday84@github.com>
2026-09-21 18:13:56 +0900

    auto: Claude 작업 자동 커밋

--- books_data.js
+++ books_data.js
@@ -796,6 +796,8 @@ const REAL_BOOKS = {
     { title: '2026 피복, 가스텅스텐, 이산화탄소가스 용접기능사 필기시험문제', author: '이동명', publisher: '크라운출판사', price: 18000, originalPrice: 20000, discount: '10%', rating: 4.7, reviews: 389, tags: ['베스트'], hasEbook: false, isbn: '9788940649749', imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788940649749.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000218570168' },
     { title: '이산화탄소가스아크 용접기능사 실기', author: '김명선', publisher: '크라운출판사', price: 16200, originalPrice: 18000, discount: '10%', rating: 4.4, reviews: 123, tags: ['추천'], hasEbook: false, isbn: '9788940649107', imageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788940649107.jpg', pageUrl: 'https://product.kyobobook.co.kr/detail/S000214378451' },
     { title: '2026 기분파 피복아크용접기능사 필기 (가스텅스텐아크용접/이산화탄소가스아크용접기능사 포함)', author: '에듀웨이 R&D 연구소', publisher: '에듀웨이', price: 20700, originalPrice: 23000, discount: '10%', tags: ['추천'], isbn: '9791194328216', imageUrl: null, pageUrl: 'https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=376256841' },
+    { title: '2026 단기완성 이산화탄소가스아크용접기능사 필기', author: '최갑규', publisher: '세진북스', price: 22500, originalPrice: 25000, discount: '10%', tags: ['추천'], hasEbook: false, isbn: '9791157458622', imageUrl: KB('9791157458622'), pageUrl: 'https://product.kyobobook.co.kr/detail/S000220241349' },
+    { title: '2026 피복아크용접기능사 필기 (가스텅스텐아크용접/이산화탄소가스아크용접기능사 포함)', author: '국가기술자격시험연구회', publisher: '예문사', price: 18900, originalPrice: 21000, discount: '10%', tags: ['추천'], hasEbook: false, isbn: '9788927461012', imageUrl: KB('9788927461012'), pageUrl: 'https://product.kyobobook.co.kr/detail/S000218964284' },
   ],
 
   '전산응용기계제도기능사': [
